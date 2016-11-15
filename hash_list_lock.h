774ffbebe4c898cecc67ee6dab84d5d0e78ca689
YangChen93 <robbie2012dou@gmail.com>
2016-11-15 10:29:02 -0500

    globallock + transaction memory + list lock

---
+++ hash_list_lock.h
@@ -0,0 +1,119 @@
+
+#ifndef HASH_H
+#define HASH_H
+
+#include <stdio.h>
+#include "list.h"
+
+#define HASH_INDEX(_addr,_size_mask) (((_addr) >> 2) & (_size_mask))
+
+template<class Ele, class Keytype> class hash;
+
+template<class Ele, class Keytype> class hash {
+ private:
+  unsigned my_size_log;
+  unsigned my_size;
+  unsigned my_size_mask;
+  list<Ele,Keytype> *entries;
+  list<Ele,Keytype> *get_list(unsigned the_idx);
+  pthread_mutex_t *mutex;
+
+ public:
+  void setup(unsigned the_size_log=5);
+  void insert(Ele *e);
+  Ele *lookup(Keytype the_key);
+  void print(FILE *f=stdout);
+  void reset();
+  void cleanup();
+  void lock_list(Keytype the_key);
+  void unlock_list(Keytype the_key);
+};
+
+template<class Ele, class Keytype> 
+void 
+hash<Ele, Keytype>::setup(unsigned the_size_log) {
+	my_size_log = the_size_log;
+	my_size = 1 << my_size_log;
+	my_size_mask = (1 << my_size_log) - 1;
+	entries = new list<Ele, Keytype>[my_size];
+	int i;
+	lock = new pthread_mutex_t[my_size];
+
+	for (i = 0; i < my_size; i++) {
+		if (pthread_mutex_init(&lock[i], NULL) != 0) {
+			printf("\n mutex init failed\n");
+			return 1;
+		}
+	}
+}
+
+template<class Ele, class Keytype> 
+list<Ele,Keytype> *
+hash<Ele,Keytype>::get_list(unsigned the_idx){
+  if (the_idx >= my_size){
+    fprintf(stderr,"hash<Ele,Keytype>::list() public idx out of range!\n");
+    exit (1);
+  }
+  return &entries[the_idx];
+}
+
+template<class Ele, class Keytype> 
+Ele *
+hash<Ele,Keytype>::lookup(Keytype the_key){
+  list<Ele,Keytype> *l;
+
+  l = &entries[HASH_INDEX(the_key,my_size_mask)];
+  lock_list(the_key);
+  return l->lookup(the_key);
+  unlock_list(the_key);
+}  
+
+template<class Ele, class Keytype> 
+void 
+hash<Ele,Keytype>::print(FILE *f){
+  unsigned i;
+
+  for (i=0;i<my_size;i++){
+    entries[i].print(f);
+  }
+}
+
+template<class Ele, class Keytype> 
+void 
+hash<Ele,Keytype>::reset(){
+  unsigned i;
+  for (i=0;i<my_size;i++){
+    entries[i].cleanup();
+  }
+}
+
+template<class Ele, class Keytype> 
+void 
+hash<Ele,Keytype>::cleanup(){
+  unsigned i;
+  reset();
+  delete [] entries;
+}
+
+template<class Ele, class Keytype> 
+void 
+hash<Ele,Keytype>::insert(Ele *e){
+	lock_list(e->key());
+  entries[HASH_INDEX(e->key(),my_size_mask)].push(e);
+  unlock_list(e->key());
+}
+
+template<class Ele, class Keytype>
+void
+hash <Ele, Keytepe>::lock_list(Keytype the_key) {
+	pthread_mutex_lock(&mutex[HASH_INDEX(the_key, my_size_mask)]);
+}
+
+template<class Ele, class Keytype>
+void
+hash <Ele, Keytepe>::unlock_list(Keytype the_key) {
+	pthread_mutex_unlock(&mutex[HASH_INDEX(the_key, my_size_mask)]);
+}
+
+
+#endif
---
+++ randtrack_tm.cc
@@ -0,0 +1,174 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#include "defs.h"
+#include "hash.h"
+
+#define SAMPLES_TO_COLLECT   10000000
+#define RAND_NUM_UPPER_BOUND   100000
+#define NUM_SEED_STREAMS            4
+
+/* 
+ * ECE454 Students: 
+ * Please fill in the following team struct 
+ */
+team_t team = {
+    "First Blood",                  /* Team name */
+
+    "Yang Chen",                    /* First member full name */
+    "999551783",                 /* First member student number */
+    "robbie.chen@mail.utoronto.ca",                 /* First member email address */
+
+    "Zhongyang Xiao",                           /* Second member full name */
+    "99",                           /* Second member student number */
+    "sam.xiao@mail.utoronto.ca"                            /* Second member email address */
+};
+
+unsigned num_threads;
+unsigned samples_to_skip;
+
+class sample;
+class argument;
+
+class sample {
+  unsigned my_key;
+ public:
+  sample *next;
+  unsigned count;
+
+  sample(unsigned the_key){my_key = the_key; count = 0;};
+  unsigned key(){return my_key;}
+  void print(FILE *f){printf("%d %d\n",my_key,count);}
+};
+
+
+class argument {
+public:
+	int startpoint;			 /*starting point of each thread*/
+	int num_stream;			/*the number of seed streams that each thread have*/
+};
+
+// This instantiates an empty hash table
+// it is a C++ template, which means we define the types for
+// the element and key value here: element is "class sample" and
+// key value is "unsigned".  
+hash<sample,unsigned> h;
+
+void *process (void *ptr);
+pthread_mutex_t fastmutex = PTHREAD_MUTEX_INITIALIZER;   //mutex lock
+
+int main (int argc, char* argv[]){
+  // Print out team information
+  printf( "Team Name: %s\n", team.team );
+  printf( "\n" );
+  printf( "Student 1 Name: %s\n", team.name1 );
+  printf( "Student 1 Student Number: %s\n", team.number1 );
+  printf( "Student 1 Email: %s\n", team.email1 );
+  printf( "\n" );
+  printf( "Student 2 Name: %s\n", team.name2 );
+  printf( "Student 2 Student Number: %s\n", team.number2 );
+  printf( "Student 2 Email: %s\n", team.email2 );
+  printf( "\n" );
+
+  // Parse program arguments
+  if (argc != 3){
+    printf("Usage: %s <num_threads> <samples_to_skip>\n", argv[0]);
+    exit(1);  
+  }
+  sscanf(argv[1], " %d", &num_threads); // not used in this single-threaded version
+  sscanf(argv[2], " %d", &samples_to_skip);
+
+if (num_threads != 1 & num_threads != 2 & num_threads !=4){
+	printf("please enter correct number of threads, it should be 1, 2 or4");
+	exit(1);
+}
+  // initialize a 16K-entry (2**14) hash of empty lists
+  h.setup(14);
+
+//create pthread
+	pthread_t thread[num_threads];
+	argument *p = new argument[num_threads];
+	int i, start = 0;
+
+
+if (num_threads ==2) {
+	for (i=0; i<2; i++){
+		p[i].startpoint = start;
+		p[i].num_stream = 2;
+		pthread_create(&thread[i], NULL, process, (void*) &p[i]);
+		start +=2;
+	}
+}
+
+if (num_threads == 4) {
+	for (i=0; i<4; i++){
+		p[i].startpoint = start;
+		p[i].num_stream = 1;
+		pthread_create(&thread[i], NULL, process, (void*) &p[i]);
+		start +=1;
+	}
+}
+
+if(num_threads == 1) {
+	p[0].startpoint = start;
+	p[0].num_stream = 4;
+	pthread_create(&thread[0], NULL, process, (void*)&p[0]);
+}
+
+for (i =0; i < num_threads; i++){
+	pthread_join(thread[i], NULL);
+}
+
+h.print();
+
+
+}
+
+void *process (void *ptr){
+	argument *p = (argument*) ptr;
+
+  int i,j,k;
+  int rnum;
+  unsigned key;
+  sample *s;
+
+  // process streams starting with different initial numbers
+  for (i = p->startpoint; i < (p->startpoint + p->num_stream); i++) {
+	  rnum = i;
+
+
+	  
+
+
+  // collect a number of samples
+	  for (j = 0; j < SAMPLES_TO_COLLECT; j++) {
+
+		  // skip a number of samples
+		  for (k = 0; k < samples_to_skip; k++) {
+			  rnum = rand_r((unsigned int*)&rnum);
+		  }
+
+		  // force the sample to be within the range of 0..RAND_NUM_UPPER_BOUND-1
+		  key = rnum % RAND_NUM_UPPER_BOUND;
+		  
+		  __transaction_atomic{
+			  // if this sample has not been counted before
+			  if (!(s = h.lookup(key))) {
+
+				  // insert a new element for it into the hash table
+				  s = new sample(key);
+				  h.insert(s);
+			  }
+
+		  // increment the count for the sample
+		  s->count++;
+
+		  }
+	  }
+  }
+
+  // print a list of the frequency of all samples
+  
+}
